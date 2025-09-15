c43e7b0fefe1f75fde2f8ac803847fa96618e686
123yyyyyuuuuu <oeaeooo@gmail.com>
2025-09-15 20:12:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const bot = mineflayer.createBot({
   host: 'oeae123666.aternos.me', // عنوان السيرفر
   port: 25171,                   // البورت
   username: 'MineScout',         // اسم الحساب (بدون كلمة مرور لأن السيرفر offline)
-  // password: 'password',       // فقط إذا كان السيرفر في online-mode
+  version: 1.21.8
 });
 
 bot.on('spawn', () => {
